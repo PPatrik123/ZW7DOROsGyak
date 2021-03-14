9b9ccd945ca1a9318d271c285b79b5e7eec13157
PPatrik123 <78854607+PPatrik123@users.noreply.github.com>
2021-03-14 21:02:09 +0100

    Add files via upload

---
+++ ZW7DOR_0310/child/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+    int i = 0;
+    for( i = 0; i < 5; i++ ){
+        printf("Palko Patrik David\n");
+        printf("ZW7DOR\n");
+    }
+    return 0;
+}
